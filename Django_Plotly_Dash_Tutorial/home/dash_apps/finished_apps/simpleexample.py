a8011394648beae348d5ee4e9d38011b3690d195
William Santos <william.wapsantos@gmail.com>
2023-02-16 14:00:17 -0300

    Atualizacao simpleexample.py

--- Django_Plotly_Dash_Tutorial/home/dash_apps/finished_apps/simpleexample.py
+++ Django_Plotly_Dash_Tutorial/home/dash_apps/finished_apps/simpleexample.py
@@ -1,53 +1,47 @@
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import Dash, html, dcc
 from dash.dependencies import Input, Output
-import plotly.graph_objs as go
+import plotly.express as px
+import plotly.graph_objects as go
+import numpy as np
+from PIL import Image
+import pydicom
 from django_plotly_dash import DjangoDash
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = DjangoDash('SimpleExample', external_stylesheets=external_stylesheets)
 
+img_path = './static/Y1_pfDicom.dcm'
+ds = pydicom.filereader.read_file(img_path)
+image_array = ds.pixel_array
 
-app.layout = html.Div([
-    html.H1('Square Root Slider Graph'),
-    dcc.Graph(id='slider-graph', animate=True, style={"backgroundColor": "#1a2d46", 'color': '#ffffff'}),
-    dcc.Slider(
-        id='slider-updatemode',
-        marks={i: '{}'.format(i) for i in range(20)},
-        max=20,
-        value=2,
-        step=1,
-        updatemode='drag',
-    ),
-])
+sad = ds[0x3002, 0x0022].value
+sid = ds[0x3002,0x0026].value
+x_res = (ds[0x3002,0x0011].value[0])*(sad/sid)
+y_res = (ds[0x3002,0x0011].value[1])*(sad/sid)
 
+dx = 12.75
+dy = 21.3
 
-@app.callback(
-               Output('slider-graph', 'figure'),
-              [Input('slider-updatemode', 'value')])
-def display_value(value):
+x_axis = [(x*x_res/10)-dx for x in range(np.shape(image_array)[0])]
+y_axis = [(y*y_res/10)-dy for y in range(np.shape(image_array)[1])]
 
 
-    x = []
-    for i in range(value):
-        x.append(i)
 
-    y = []
-    for i in range(value):
-        y.append(i*i)
+fig = px.imshow(image_array, x=x_axis, y=y_axis, origin='lower')
+fig.update_layout(coloraxis_showscale=False)
 
-    graph = go.Scatter(
-        x=x,
-        y=y,
-        name='Manipulate Graph'
-    )
-    layout = go.Layout(
-        paper_bgcolor='#27293d',
-        plot_bgcolor='rgba(0,0,0,0)',
-        xaxis=dict(range=[min(x), max(x)]),
-        yaxis=dict(range=[min(y), max(y)]),
-        font=dict(color='white'),
+#Horizontal and Vertical Lines and Rectangles in Python
+#https://plotly.com/python/horizontal-vertical-shapes/
+fig.add_vline(x=0, line_width=1, line_dash="dash", line_color="green")
+fig.add_hline(y=0, line_width=1, line_dash="dash", line_color="green")
 
-    )
-    return {'data': [graph], 'layout': layout}
+
+app.layout = html.Div(children=[
+                        html.H1(children='Hello Dash'),
+                        html.Div(children='''Dash: A web application framework for your data'''),
+                        dcc.Graph(
+                                    id='example-graph',
+                                    figure=fig
+                                )
+                    ])
